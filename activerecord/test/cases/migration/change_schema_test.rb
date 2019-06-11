745f201792edab75ff685e13eeb2469de458912c
Jesse Chavez <jesse.chavez.r@gmail.com>
2019-06-11 21:13:38 +1000

    mssql cannot recreate referenced table with force: :cascade, but this adapter mimics this behaivour

--- activerecord/test/cases/migration/change_schema_test.rb
+++ activerecord/test/cases/migration/change_schema_test.rb
@@ -454,6 +454,10 @@ module ActiveRecord
             @connection.create_table :trains, force: true
           end
 
+          # NOTE: mssql cannot recreate referenced table with force: :cascade
+          # but this is mimic in the adapter.
+          # https://docs.microsoft.com/en-us/sql/t-sql/statements/drop-table-transact-sql?view=sql-server-2017
+          #
           # can recreate referenced table with force: :cascade
           @connection.create_table :trains, force: :cascade
           assert_equal [], @connection.foreign_keys(:wagons)
