c49cd3a8fdc68599a0e73abf5b49bdbde3efcf1a
Jesse Chavez <jesse.chavez.r@gmail.com>
2019-06-22 14:39:39 +1000

    [mssql] does not support 1=2 or 2=2

--- activerecord/test/cases/attribute_methods_test.rb
+++ activerecord/test/cases/attribute_methods_test.rb
@@ -548,7 +548,7 @@ class AttributeMethodsTest < ActiveRecord::TestCase
   test "typecast attribute from select to false" do
     Topic.create(title: "Budget")
     # Oracle does not support boolean expressions in SELECT.
-    if current_adapter?(:OracleAdapter, :FbAdapter)
+    if current_adapter?(:OracleAdapter, :FbAdapter, :MSSQLAdapter)
       topic = Topic.all.merge!(select: "topics.*, 0 as is_test").first
     else
       topic = Topic.all.merge!(select: "topics.*, 1=2 as is_test").first
@@ -559,7 +559,7 @@ class AttributeMethodsTest < ActiveRecord::TestCase
   test "typecast attribute from select to true" do
     Topic.create(title: "Budget")
     # Oracle does not support boolean expressions in SELECT.
-    if current_adapter?(:OracleAdapter, :FbAdapter)
+    if current_adapter?(:OracleAdapter, :FbAdapter, :MSSQLAdapter)
       topic = Topic.all.merge!(select: "topics.*, 1 as is_test").first
     else
       topic = Topic.all.merge!(select: "topics.*, 2=2 as is_test").first
