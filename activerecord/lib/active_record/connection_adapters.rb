4b700ea8bfdac68a6674fb7882890baa7e9987c2
Jesse Chavez <jesse.chavez.r@gmail.com>
2026-09-16 21:13:28 +0900

    [mssql] register sqlserver adapter temporarily

--- activerecord/lib/active_record/connection_adapters.rb
+++ activerecord/lib/active_record/connection_adapters.rb
@@ -69,6 +69,8 @@ module ActiveRecord
     register "trilogy", "ActiveRecord::ConnectionAdapters::TrilogyAdapter", "active_record/connection_adapters/trilogy_adapter"
     register "postgresql", "ActiveRecord::ConnectionAdapters::PostgreSQLAdapter", "active_record/connection_adapters/postgresql_adapter"
 
+    register("sqlserver", "ActiveRecord::ConnectionAdapters::MSSQLAdapter", "active_record/connection_adapters/mssql_adapter")
+
     eager_autoload do
       autoload :AbstractAdapter
     end
